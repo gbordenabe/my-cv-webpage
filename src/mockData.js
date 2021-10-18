d367b09dd82b62fa539f907df2df37b5e60d493e
Gabriel <gabrielbordenabe@gmail.com>
2021-10-18 17:50:16 -0300

    works added

--- src/mockData.js
+++ src/mockData.js
@@ -1,5 +1,7 @@
 import giffinder from "./static/giffinder.png";
 import hkecommerce from "./static/hkecommerce.png";
+import todolistapp from "./static/todolistapp.png";
+import moviesapp from "./static/moviesapp.png";
 
 const mockData = [
   {
@@ -13,14 +15,14 @@ const mockData = [
     link: "https://gbordenabe.github.io/giffinder/",
   },
   {
-    title: "Gif Finder",
-    image: `./${giffinder}`,
-    link: "https://gbordenabe.github.io/giffinder/",
+    title: "To Do List",
+    image: `./${todolistapp}`,
+    link: "https://gbordenabe.github.io/to-do-list-app/",
   },
   {
-    title: "Gif Finder",
-    image: `./${giffinder}`,
-    link: "https://gbordenabe.github.io/giffinder/",
+    title: "Movies App",
+    image: `./${moviesapp}`,
+    link: "https://gbordenabe.github.io/movies-app/",
   },
   {
     title: "Gif Finder",
